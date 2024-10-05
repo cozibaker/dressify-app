88879bcb612a4d7092bc8a37942a3255f481d53f
cozibaker <cozidan2000@gmail.com>
2024-10-05 14:21:59 -0700

    Added user table to database

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from database_functions import database_worker
 
-userDatabase = database_worker("userDatabase")
+userDatabase = database_worker("user.db")
 
-clothingDatabase = database_worker("clothingDatabase")
+clothingDatabase = database_worker("clothing.db")
+
+def create_database():
+    db = userDatabase
+    query_user = '''CREATE table if not exists user (
+        id INTEGER PRIMARY KEY,
+        username TEXT,
+        name TEXT,
+        email TEXT,
+        password TEXT
+    )'''
+
