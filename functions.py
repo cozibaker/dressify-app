22c09f8b6ad936b393497c502e76547354c483c5
cozibaker <cozidan2000@gmail.com>
2024-10-05 13:12:58 -0700

    Added database operators

---
+++ functions.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class database_worker:
+    def __init__(self, name):
+        self.connection = sqlite3.connect(name)
+        self.cursor = self.connection.cursor()
+
+    def search(self, query):
+        result = self.cursor.execute(query).fetchall()
+        return result
+
+    def run_save(self, query):
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
+
+    def run_fetchone(self, query):
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        return result
